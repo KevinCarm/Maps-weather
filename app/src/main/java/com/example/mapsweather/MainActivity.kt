1aaddfc95af1d6852f2ace9a794a46c63aecbb42
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-10-18 13:14:04 -0500

    First commit and maps dependendy added

---
+++ app/src/main/java/com/example/mapsweather/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mapsweather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+    }
+
+}
