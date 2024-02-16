a194a457341342fe259f420dd8806baabd3046f9
KevinCarm <kevincarm016@hotmail.com>
2024-02-16 17:05:19 -0600

    Make RetrofitHerper class as singleton

--- app/src/main/java/com/example/mapsweather/data/api/RetrofitHelper.kt
+++ app/src/main/java/com/example/mapsweather/data/api/RetrofitHelper.kt
@@ -5,11 +5,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
     private const val baseUrl = "https://api.openweathermap.org/"
-
+    private var retrofit: Retrofit? = null
     fun getInstance(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        return if(retrofit != null)
+            retrofit!!
+        else {
+            val obj = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            retrofit = obj
+            retrofit!!
+        }
     }
 }
